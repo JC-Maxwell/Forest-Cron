22d340369390c89d3fa29cfa23d3b7d100c7b6bc
Hugo Enrique Ochoa Heredia <hugo.ochoa.h@gmail.com>
2015-08-28 20:21:15 -0500

    Firmware.ISA consumed locally

--- General/firmware.py
+++ General/firmware.py
@@ -17,7 +17,6 @@ import json
 
 # EXTERNAL
 import logging
-import requests
 
 # SDK:
 from pauli_sdk.Modules import constants as _Pauli_Constants
@@ -29,12 +28,12 @@ from pauli_sdk.Classes.response import Already_Handled_Exception
 from General import constants as _Constants
 from General import utilities as _Utilities
 
+# Firmware:
+from forest_firmware.isa import instructions as ISA
+
 # ======================================================== MODULE CODE
 
 LOG_INDENT = _Constants.LOG_INDENT
-
-# Firmware url:
-FIRMWARE_URL = _Constants.FIRMWARE_URL
 AVOID_FIRMWARE = _Constants.AVOID_FIRMWARE
 DEFAULT_FIRMWARE_RESULT = _Constants.DEFAULT_FIRMWARE_RESULT
 FIRMWARE_STABLISH_CONNECTION_TIME_ON_SECONDS = _Constants.FIRMWARE_STABLISH_CONNECTION_TIME
@@ -48,39 +47,28 @@ def isa(**params):
 			del params['logger']
 		taxpayer = params['taxpayer']
 		del params['taxpayer']
-		url = FIRMWARE_URL
-		# url = 'http://fakeurl'
 		log = params['log']
 		del params['log']
 		payload = params
 		headers = {'content-type':'application/json'}
 		# AVOID_FIRMWARE = True
 		if AVOID_FIRMWARE is not True:
-			try:
-				logger.info(3*LOG_INDENT + 'Requesting ' + url + ' ( ' + str(FIRMWARE_WAITING_TIME_ON_SECONDS) + ' secs ) ... ')
-				firmware_result = requests.post(url, data=json.dumps(payload), headers=headers,timeout=(FIRMWARE_STABLISH_CONNECTION_TIME_ON_SECONDS,FIRMWARE_WAITING_TIME_ON_SECONDS))# timeout = (connection,read)
+			try:				
+				instruction = params['instruction']
+				isa_params = params['params']
+				logger.info(3*LOG_INDENT + 'Consuming Firmware.ISA ')
+				firmware_result = ISA[instruction](isa_params)
+				firmware_result_json = firmware_result.content
+				log['firmware']['new'] = len(firmware_result_json['new'])
+				log['firmware']['update'] = len(firmware_result_json['updated'])
+				_Utilities.handle_forest_cron_success('ok',logger=logger)
+				return firmware_result_json
 			except Exception as e:
 				logger.info(e)
 				logger.info(3*LOG_INDENT + 'Request to firmware was not possible (connection or timeout)')
 				logger.info(3*LOG_INDENT + 'Avoiding iteration ... ')
 				log['avoid_iteration'] = True# Just a flag to avoid taxpayer logs to be updated with this sync/init iteration (they must not be updated because there was a firmware problem)
 				_Utilities.handle_forest_cron_error(e,logger=logger)
-				return DEFAULT_FIRMWARE_RESULT
-			if firmware_result.status_code == 200:
-				_Utilities.handle_forest_cron_success('ok',logger=logger)
-				firmware_result_json = firmware_result.json()# Comment in case of simulation
-				log['firmware']['new'] = len(firmware_result_json['new'])
-				log['firmware']['update'] = len(firmware_result_json['updated'])
-				return firmware_result_json
-			else:
-				if logger is not None:
-					logger.critical('Firmware error')
-					logger.critical(firmware_result)
-				logger.info(e)
-				logger.info(3*LOG_INDENT + 'Firmware return error')
-				logger.info(3*LOG_INDENT + 'Avoiding iteration ... ')
-				log['avoid_iteration'] = True# Just a flag to avoid taxpayer logs to be updated with this sync/init iteration (they must not be updated because there was a firmware problem)
-				_Utilities.handle_forest_cron_error('Firmware.response.code was not 200',logger=logger)
 				return DEFAULT_FIRMWARE_RESULT
 		else:
 			_Utilities.handle_forest_cron_success('ok',logger=logger)
@@ -93,7 +81,6 @@ def isa(**params):
 		# _Utilities.update_taxpayer_firmware_timeout(taxpayer,logger=logger)# firmware timeout updating was avoided
 		# -------------------------------------------------------------------
 		raise e
-	
 
 
 
